359f3337614f6a618f0aa77a80c97cda892dfa73
Dmambo <mambotrader0@gmail.com>
2023-10-10 20:37:33 +0530

    Added the navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main>
+  
+      <Button variant={"destructive"} className="m-8">
       hello next
+      </Button>
+
     </main>
   )
 }
